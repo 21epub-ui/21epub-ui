57c5b139d35ab7e6bcdd5440fc8cd2c841eac5d4
ongyuxing <78018704+ongyuxing@users.noreply.github.com>
2022-02-15 19:32:02 +0800

    feat: add QuickAccess component

---
+++ packages/quick-access/src/components/QuickAccessMenu/styles.ts
@@ -0,0 +1,20 @@
+import { grey } from '@ant-design/colors'
+import { Menu } from 'antd'
+import styled from 'styled-components'
+
+export const MenuItemGroup = styled(Menu.ItemGroup)`
+  font-size: 12px;
+`
+
+export const MenuItem = styled(Menu.Item)`
+  color: ${grey[5]};
+  font-size: 12px;
+  height: 32px;
+  overflow: hidden;
+  white-space: normal;
+
+  .ant-dropdown-menu-title-content {
+    display: flex;
+    align-items: center;
+  }
+`
---
+++ packages/quick-access/stories/QuickAccess.stories.tsx
@@ -0,0 +1,16 @@
+import type { ComponentMeta, ComponentStory } from '@storybook/react'
+import 'antd/dist/antd.css'
+import { QuickAccess } from '../src'
+
+export default {
+  title: 'QuickAccess/Default',
+  component: QuickAccess,
+} as ComponentMeta<typeof QuickAccess>
+
+const Template: ComponentStory<typeof QuickAccess> = (args) => {
+  return <QuickAccess {...args} />
+}
+
+export const Default = Template.bind({})
+
+Default.args = { type: 'cbt' }
