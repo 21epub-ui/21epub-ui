c4be593140b9f1bcaa3ee195d1caa1370fb76d2f
ongyuxing <78018704+ongyuxing@users.noreply.github.com>
2022-02-16 17:33:11 +0800

    chore: remove scripts field on release

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -1,4 +1,6 @@
 import { execute } from '@yarnpkg/shell'
+import fs from 'fs-extra'
+import path from 'path'
 import simpleGit from 'simple-git'
 
 const git = simpleGit()
@@ -8,6 +10,20 @@ const tagName = tags.latest
 const packageName = tagName?.match(/^@.+\/.+(?=@(?:\d+\.){2}\d+)/)[0]
 
 if (packageName !== undefined) {
+  const dirPath = path.resolve('packages', packageName.split('/')[1])
+  const filePath = path.resolve(dirPath, 'package.json')
+
+  const packageConfig = await fs.readJSON(filePath)
+
   await execute(`yarn workspace ${packageName} run build`)
+
+  await fs.outputJson(
+    filePath,
+    { ...packageConfig, scripts: undefined, devDependencies: undefined },
+    { spaces: 2 }
+  )
+
   await execute(`yarn workspace ${packageName} npm publish`)
+
+  await fs.outputJson(filePath, packageConfig, { spaces: 2 })
 }
