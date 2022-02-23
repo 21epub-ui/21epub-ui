5b4dacfc6fca0dea8d316411f1876cc38b1311bd
ongyuxing <78018704+ongyuxing@users.noreply.github.com>
2022-02-23 18:01:05 +0800

    feat: add strict mode to stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,7 @@
+import { StrictMode } from 'react'
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
@@ -7,3 +9,11 @@ export const parameters = {
     },
   },
 }
+
+export const decorators = [
+  (Story) => (
+    <StrictMode>
+      <Story />
+    </StrictMode>
+  ),
+]
