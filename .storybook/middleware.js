f3e8a27f5441057cb7e3fad7417fe37721bde32b
ongyuxing <78018704+ongyuxing@users.noreply.github.com>
2022-07-13 20:20:52 +0800

    fix: update storybook config

--- .storybook/middleware.js
+++ .storybook/middleware.js
@@ -2,6 +2,13 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = (app) => {
   app.use(
+    '/epub360-media',
+    createProxyMiddleware({
+      target: 'https://cbt.21epub.com:8443',
+      changeOrigin: true,
+    })
+  )
+  app.use(
     '/v2',
     createProxyMiddleware({
       target: 'https://cbt.21epub.com:8443',
