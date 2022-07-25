b7088d95aae2c6bf592cde2e051da15e291e427a
ongyuxing <78018704+ongyuxing@users.noreply.github.com>
2022-07-25 18:04:03 +0800

    fix: update build script

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,8 +1,5 @@
-import fs from 'fs-extra'
-import { resolve } from 'path'
 import { execute } from '@yarnpkg/shell'
 
 const { PROJECT_CWD } = process.env
 
-await fs.emptyDir(resolve('dist'))
 await execute(`yarn g:rollup -c ${PROJECT_CWD}/rollup.config.js`)
