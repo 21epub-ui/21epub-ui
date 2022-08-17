ba4102a13d909d9bf1fdccbe0e5e5bdab22753d5
ongyuxing <78018704+ongyuxing@users.noreply.github.com>
2022-08-17 12:27:26 +0800

    chore: update outputJson helper

--- scripts/utils/outputJson.mjs
+++ scripts/utils/outputJson.mjs
@@ -1,7 +1,7 @@
 import fs from 'fs-extra'
 
-const outputJson = (file, object, options, callback) => {
-  return fs.outputJson(file, object, { spaces: 2, ...options }, callback)
+const outputJson = (file, object, options) => {
+  return fs.outputJson(file, object, { spaces: 2, ...options })
 }
 
 export default outputJson
