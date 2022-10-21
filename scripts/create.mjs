07888841d33a80602a9c8a15b2d8d01e0482756d
ongyuxing <78018704+ongyuxing@users.noreply.github.com>
2022-10-21 18:19:42 +0800

    chore: update create script

--- scripts/create.mjs
+++ scripts/create.mjs
@@ -1,26 +1,20 @@
-import fs from 'fs-extra'
-import { resolve } from 'path'
 import genIndex from './generators/genIndex.mjs'
 import genPackage from './generators/genPackage.mjs'
 import genReadMe from './generators/genReadMe.mjs'
 import genStories from './generators/genStories.mjs'
 import genTemplate from './generators/genTemplate.mjs'
+import genTsConfig from './generators/genTsConfig.mjs'
 import genTypes from './generators/genTypes.mjs'
-import getPackagePath from './utils/getPackagePath.mjs'
 
 const argv = process.argv.slice(2)
 const [name] = argv
 
 if (name !== undefined) {
   await genPackage()
+  await genTsConfig()
   await genTemplate()
   await genStories()
   await genTypes()
   await genIndex()
   await genReadMe()
-
-  await fs.symlink(
-    resolve(getPackagePath('root'), 'shared', 'tsconfig.json'),
-    resolve(getPackagePath(name), 'tsconfig.json')
-  )
 }
