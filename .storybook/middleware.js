cf3da42ef16bb8d69fa98ae03d003aa551dccc40
ongyuxing <78018704+ongyuxing@users.noreply.github.com>
2023-01-09 18:06:27 +0800

    chore: update proxy config

--- .storybook/middleware.js
+++ .storybook/middleware.js
@@ -4,22 +4,25 @@ module.exports = (app) => {
   app.use(
     '/epub360-media',
     createProxyMiddleware({
-      target: 'https://cbt.21epub.com:8443',
+      target: 'https://cbt.21epub.com/',
       changeOrigin: true,
+      secure: false,
     })
   )
   app.use(
     '/v2',
     createProxyMiddleware({
-      target: 'https://cbt.21epub.com:8443',
+      target: 'https://cbt.21epub.com/',
       changeOrigin: true,
+      secure: false,
     })
   )
   app.use(
     '/v3',
     createProxyMiddleware({
-      target: 'https://cbt.21epub.com:8443',
+      target: 'https://cbt.21epub.com/',
       changeOrigin: true,
+      secure: false,
     })
   )
 }
